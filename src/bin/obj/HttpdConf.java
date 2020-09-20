bca1d5148838f62a562632863230e423cc0900fd
adamjbea <adamjbea@gmail.com>
2020-09-20 13:35:32 -0700

    Htaccess Data Object
    Data object class for htaccess finished, removed unused import statements

--- src/bin/obj/HttpdConf.java
+++ src/bin/obj/HttpdConf.java
@@ -1,10 +1,6 @@
 package bin.obj;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.StringTokenizer;
 
 public class HttpdConf{
 
