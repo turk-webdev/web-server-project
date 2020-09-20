52a9d08fa76a5ad4b368643c97bb4baaa6ba54da
adamjbea <adamjbea@gmail.com>
2020-09-20 13:21:37 -0700

    Added Skeletons, finished Htaccess Parser
    Parser finished for htaccess, rest of the classes (htpwd parser, htaccess and htpwd dataobj, and auth) to come soon

---
+++ src/bin/obj/Htpassword.java
@@ -0,0 +1,4 @@
+package bin.obj;
+
+public class Htpassword {
+}
---
+++ src/bin/obj/parser/HtpasswordParser.java
@@ -0,0 +1,11 @@
+package bin.obj.parser;
+
+import bin.obj.Htpassword;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class HtpasswordParser {
+}
